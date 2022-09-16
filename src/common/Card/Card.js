7687815564dd98e7a23ac7fa8cecade46d542c93
satyam4p <satyamkumar343@gmail.com>
2022-09-16 23:04:37 +0530

    WIP:making card component hoverable for crud

--- src/common/Card/Card.js
+++ src/common/Card/Card.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './stylesheet.scss';
 import { Card as AntCard } from 'antd';
 
-const style={
-    'width': '180px',
-    'min-height': '100px',
-    'border':'1px solid black',
-    'padding':'10px',
-    'margin-bottom':'10px',
-
-}
-
 const Card=(props)=>{
+
+    const [hoverElement, setHoverElement] = useState(false); 
+
+    console.log("hover element: ",hoverElement);
     return(
-        <AntCard
-            draggable={true}
-            title={ props.task.name}
-            style={style}
-            onDragStart={e=>{
-                e.dataTransfer.setData("taskID", props.task.id);
-                e.dataTransfer.setData("fromCol", props.task.status);
-            }}
-            onDragOver={e=>e.preventDefault}
-        />
+        <div>
+            <AntCard.Meta
+                className = {`ant-card-meta ${hoverElement ? 'hover' : '' }`}
+                onMouseOver={(event)=>{
+                    event.preventDefault();
+                    setHoverElement(true);
+                }}
+                onMouseLeave={(event)=>{
+                    event.preventDefault();
+                    setHoverElement(false);
+                }}
+                draggable={true}
+                title={ props.task.name}
+                onDragStart={e=>{
+                    e.dataTransfer.setData("taskID", props.task.id);
+                    e.dataTransfer.setData("fromCol", props.task.status);
+                }}
+                onDragOver={e=>e.preventDefault}
+            />
+        </div>
     )
 }
 
