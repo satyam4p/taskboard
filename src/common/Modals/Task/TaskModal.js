41ebc5a33c8e0eb5c1a9b101763b0fade2eb13be
satyam4p <satyamkumar343@gmail.com>
2023-01-02 16:50:44 +0530

    moving task modal to parent view

--- src/common/Modals/Task/TaskModal.js
+++ src/common/Modals/Task/TaskModal.js
@@ -4,7 +4,6 @@ import React,{Suspense, useEffect, useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchTasks, addTask, selectAllTasks, selectError, selectStatus } from "../../../features/task/taskSlice";
 /** END */
-import useModals from "../../../helpers/hooks/useModals";
 import { Box, Container, Flex, Input, Label, Text } from "theme-ui";
 import iconsMap from "../../Icons/iconsMap";
 import TextLoader from "../../Loaders/simpleTextLoader";
@@ -67,7 +66,6 @@ const TaskModal=(props)=>{
         }
 
     ]
-    const { modalType, setModalType } = useModals();
     const [activeTab, setActiveTab] = useState('comments');
 
     const dispatch = useDispatch();
@@ -96,199 +94,197 @@ const TaskModal=(props)=>{
 
     }
 
-    if(modalType.type === 'task' && modalType.isVisible){
-        return(
-            <div sx={{
-                zIndex:'400',
-                width:'40%',
-                height:'80vh',
-                position:'absolute',
-                top:'50%',
-                left:'50%',
-                transform:'translate(-50%,-45%)',
-                borderRadius:'5px',
-                boxShadow: '0px 1px 5px 0px rgba(0,0,0,0.75)',
-                overflowY:'auto',
-                fontSize:1
-            }}>
-                <Box as={'form'} onSubmit = {event=>handleSaveSubmit(event)}>
+    return(
+        <div sx={{
+            zIndex:'400',
+            width:'40%',
+            height:'80vh',
+            position:'absolute',
+            top:'50%',
+            left:'50%',
+            transform:'translate(-50%,-45%)',
+            borderRadius:'5px',
+            boxShadow: '0px 1px 5px 0px rgba(0,0,0,0.75)',
+            overflowY:'auto',
+            fontSize:1
+        }}>
+            <Box as={'form'} onSubmit = {event=>handleSaveSubmit(event)}>
+                <div sx={{
+                    display:'flex',
+                    alignItems:'center',
+                    justifyContent:'end',
+                    margin:'15px',
+                }}>
+                    <div sx={{
+                        margin:'10px',
+                        '&:hover':{
+                            cursor:'pointer'
+                        }
+                    }}>
+                        {<button type="submit" sx={{
+                            background:'transparent',
+                            border:'none'
+                        }}>
+                            <SaveOutlined style={{
+                            fontSize:'20px'
+                        }} />    
+                        </button>}
+                    </div>
+                    <div sx={{
+                        margin:'10px',
+                        '&:hover':{
+                            cursor:'pointer'
+                        }
+                    }}>
+                        {<button sx={{
+                            background:'transparent',
+                            border:'none'
+                        }}>
+                            <ShareAltOutlined style={{
+                                fontSize:'20px'
+                            }}/>
+                        </button>}
+                    </div>
+                    <div sx={{
+                        margin:'10px',
+                        '&:hover':{
+                            cursor:'pointer'
+                        }
+                    }}>
+                        {<button sx={{
+                            background:'transparent',
+                            border:'none'
+                        }}>
+                            <EditOutlined style={{
+                                fontSize:'20px'
+                            }}/>
+                        </button>}
+                    </div>
+                    <div sx={{
+                        margin:'10px',
+                        '&:hover':{
+                            cursor:'pointer'
+                        }
+                    }}>
+                        {<button sx={{
+                            background:'transparent',
+                            border:'none'
+                        }}>
+                            <MoreOutlined style={{
+                                fontSize:'20px'
+                            }}/>
+                        </button>}
+                    </div>
+                    <div sx={{
+                        margin:'10px',
+                        '&:hover':{
+                            cursor:'pointer'
+                        }
+                    }}
+                    onClick={()=>props.setModalType((prev)=>{
+                        return {
+                            ...prev,
+                            isVisible:false
+                        }
+                    })}
+                    >
+                        {<CloseCircleOutlined style={{
+                            fontSize:'20px'
+                        }}/>}
+                    </div>
+                </div>
+                <Label htmlFor="title"></Label>
+                <Input sx={{
+                    border:'0.5px solid #C4C4C4',
+                    padding:'10px',
+                    marginX:'10px',
+                    width:'90%',
+                    margin:'auto',
+                    height:'40px',
+                }} name="title" placeholder="Enter Title"/>
+                <Container sx={{
+                    width:'90%',
+                    marginY:'10px',
+                    display:'flex',
+                    flexDirection:'column',
+                    justifyContent:'space-between',
+                    minHeight:'10em'
+                }}>
                     <div sx={{
                         display:'flex',
                         alignItems:'center',
-                        justifyContent:'end',
-                        margin:'15px',
-                    }}>
-                        <div sx={{
-                            margin:'10px',
-                            '&:hover':{
-                                cursor:'pointer'
-                            }
-                        }}>
-                            {<button type="submit" sx={{
-                                background:'transparent',
-                                border:'none'
-                            }}>
-                                <SaveOutlined style={{
-                                fontSize:'20px'
-                            }} />    
-                            </button>}
-                        </div>
-                        <div sx={{
-                            margin:'10px',
-                            '&:hover':{
-                                cursor:'pointer'
-                            }
-                        }}>
-                            {<button sx={{
-                                background:'transparent',
-                                border:'none'
-                            }}>
-                                <ShareAltOutlined style={{
-                                    fontSize:'20px'
-                                }}/>
-                            </button>}
-                        </div>
-                        <div sx={{
-                            margin:'10px',
-                            '&:hover':{
-                                cursor:'pointer'
-                            }
-                        }}>
-                            {<button sx={{
-                                background:'transparent',
-                                border:'none'
-                            }}>
-                                <EditOutlined style={{
-                                    fontSize:'20px'
-                                }}/>
-                            </button>}
-                        </div>
-                        <div sx={{
-                            margin:'10px',
-                            '&:hover':{
-                                cursor:'pointer'
-                            }
-                        }}>
-                            {<button sx={{
-                                background:'transparent',
-                                border:'none'
-                            }}>
-                                <MoreOutlined style={{
-                                    fontSize:'20px'
-                                }}/>
-                            </button>}
-                        </div>
-                        <div sx={{
-                            margin:'10px',
-                            '&:hover':{
-                                cursor:'pointer'
-                            }
-                        }}
-                        onClick={()=>setModalType((prev)=>{
-                            return {
-                                ...prev,
-                                isVisible:false
-                            }
-                        })}
-                        >
-                            {<CloseCircleOutlined style={{
-                                fontSize:'20px'
-                            }}/>}
-                        </div>
-                    </div>
-                    <Label htmlFor="title"></Label>
-                    <Input sx={{
-                        border:'0.5px solid #C4C4C4',
-                        padding:'10px',
-                        marginX:'10px',
-                        width:'90%',
-                        margin:'auto',
-                        height:'40px',
-                    }} name="title" placeholder="Enter Title"/>
-                    <Container sx={{
-                        width:'90%',
-                        marginY:'10px',
-                        display:'flex',
                         flexDirection:'column',
-                        justifyContent:'space-between',
-                        minHeight:'10em'
+                        margin:'5px',
                     }}>
-                        <div sx={{
-                            display:'flex',
-                            alignItems:'center',
-                            flexDirection:'column',
-                            margin:'5px',
-                        }}>
-                            {fieldConfig.map((field, key)=>{
-                                return(
-                                    <FieldMapper
-                                        key = {key} 
-                                        field = {field.field}
-                                        label = {field.label}
-                                        icon =  {field.icon} 
-                                        options = {field.options}
-                                    />
-                                )
-                            })}
-                        </div>
+                        {fieldConfig.map((field, key)=>{
+                            return(
+                                <FieldMapper
+                                    key = {key} 
+                                    field = {field.field}
+                                    label = {field.label}
+                                    icon =  {field.icon} 
+                                    options = {field.options}
+                                />
+                            )
+                        })}
+                    </div>
+                </Container>
+            </Box>
+                <Container
+                    sx={{
+                        width:'90%',
+                        marginTop:'30px'
+                    }}>
+                    <div sx={{
+                        borderBottom:'0.5px solid',
+                        width:'30%',
+                        display:'flex',
+                        justifyContent:'space-between'
+                    }}>
+                        <button sx={{
+                            borderBottom:()=>activeTab=='comments' ? '2px solid #36463F' : 'none',
+                            borderTop:'none',
+                            borderLeft:'none',
+                            borderRight:'none',
+                            bg:'transparent',
+                            padding:'4px',
+                            fontSize:1,
+                            '&:hover':{
+                                bg:'#F6F6F6'
+                            },
+                        }}
+                        onClick={e=>toggleTab(e,'comments')}
+                        >
+                            <span>Comments</span>
+                        </button>
+                        <button sx={{
+                            borderBottom:()=>activeTab=='description' ? '2px solid #36463F' : 'none',
+                            borderTop:'none',
+                            borderLeft:'none',
+                            borderRight:'none',
+                            bg:'transparent',
+                            padding:'4px',
+                            fontSize:1,
+                            '&:hover':{
+                                bg:'#F6F6F6'
+                            }
+                            
+                        }}
+                        onClick={e=>toggleTab(e,'description')}
+                        >
+                            <span>Description</span>
+                        </button>
+                    </div>
+                    <Container className="data-container">
+                        <Suspense fallback={<TextLoader/>}>
+                            {activeTab === 'comments' 
+                                ? <Comments/>
+                                : null
+                            }
+                        </Suspense>
                     </Container>
-                </Box>
-                    <Container
-                        sx={{
-                            width:'90%',
-                            marginTop:'30px'
-                        }}>
-                        <div sx={{
-                            borderBottom:'0.5px solid',
-                            width:'30%',
-                            display:'flex',
-                            justifyContent:'space-between'
-                        }}>
-                            <button sx={{
-                                borderBottom:()=>activeTab=='comments' ? '2px solid #36463F' : 'none',
-                                borderTop:'none',
-                                borderLeft:'none',
-                                borderRight:'none',
-                                bg:'transparent',
-                                padding:'4px',
-                                fontSize:1,
-                                '&:hover':{
-                                    bg:'#F6F6F6'
-                                },
-                            }}
-                            onClick={e=>toggleTab(e,'comments')}
-                            >
-                                <span>Comments</span>
-                            </button>
-                            <button sx={{
-                                borderBottom:()=>activeTab=='description' ? '2px solid #36463F' : 'none',
-                                borderTop:'none',
-                                borderLeft:'none',
-                                borderRight:'none',
-                                bg:'transparent',
-                                padding:'4px',
-                                fontSize:1,
-                                '&:hover':{
-                                    bg:'#F6F6F6'
-                                }
-                                
-                            }}
-                            onClick={e=>toggleTab(e,'description')}
-                            >
-                                <span>Description</span>
-                            </button>
-                        </div>
-                        <Container className="data-container">
-                            <Suspense fallback={<TextLoader/>}>
-                                {activeTab === 'comments' 
-                                    ? <Comments/>
-                                    : null
-                                }
-                            </Suspense>
-                        </Container>
-                    </Container>
-            </div>
-        )
-    } 
+                </Container>
+        </div>
+    )
 }
 export default TaskModal;
--- src/views/home.js
+++ src/views/home.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState,Suspense } from 'react';
 import SecondaryBar from '../Components/Secondarybar/SecondaryBar';
 import SideMenu from '../Components/SideMenu/SideMenu';
 import useAuth from '../helpers/hooks/useAuth';
-import TaskModal from '../common/Modals/Task/TaskModal';
+import useModals from '../helpers/hooks/useModals';
+
+const TaskModal = React.lazy(()=>import('../common/Modals/Task/TaskModal'));
+// import TaskModal from '../common/Modals/Task/TaskModal';
 
 function Home(props){
     const [toggleSideMenu, setToggleSideMenu] = useState(true);
     const {auth} = useAuth();
     const [toggleProfile, setToggleProile] = useState(false);
     const [toggleAddMenu, setToggleAddMenu] = useState(false);
+    const { modalType, setModalType } = useModals();
 
     return(
         <>
@@ -23,7 +27,12 @@ function Home(props){
                 toggleProfile = {toggleProfile}
                 setToggleAddMenu = {setToggleAddMenu}
             />
-            <TaskModal/>
+            {
+            (modalType.type === 'task' && modalType.isVisible) &&
+                <Suspense fallback={<h4>Loading...</h4>}>
+                    <TaskModal setModalType = {setModalType}  />
+                </Suspense>   
+            }
         </>
     )
 
