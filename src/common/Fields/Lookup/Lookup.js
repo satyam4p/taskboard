14113e396d37daa84646e879ca50b84b50c100f2
satyam4p <satyamkumar343@gmail.com>
2023-01-10 23:41:15 +0530

    WIP: small changes

--- src/common/Fields/Lookup/Lookup.js
+++ src/common/Fields/Lookup/Lookup.js
@@ -4,35 +4,36 @@ import { Select, Box } from 'theme-ui';
 
 //this array will be obtained from the api call or prop
 
-const getOptions = ()=>{
-  return new Promise((resolve,reject) => setTimeout(resolve(
+const getOptions = ()=> {
+  return new Promise((resolve, reject) => setTimeout(resolve(
     ['hello',
-    'blah',
-    'hjasdv',
-    'yiqwe',
-    'Hi']
-  ),2000)); 
+      'blah',
+      'hjasdv',
+      'yiqwe',
+      'Hi']
+  ), 2000 )); 
 }
 
 const LookupField = () => {
-  const [options, setOptions] = useState([]);
-  const handleChange = ()=>{
-    console.log("change clicked");
-  }
-  const handleClick= async ()=>{
-    if(options.length > 0){
-      return;
+    const [options, setOptions] = useState([]);
+    const handleChange = () =>{
+      console.log("change clicked");
     }
-    const optionsList = await getOptions();
-    console.log("options list:: ",optionsList);
-    setOptions(optionsList);
-  }
+    const handleClick= async ()=>{
+      if(options.length > 0){
+        return;
+      }
+      const optionsList = await getOptions();
+      console.log("options list:: ",optionsList);
+      setOptions(optionsList);
+    }
 
   return(
-    <Select sx={{
-      minWidth:'120px'
-    }} defaultValue="Hello"
-    arrow={
+    <Select sx = {{
+        minWidth:'120px'
+      }} 
+    defaultValue="Hello"
+    arrow = {
       <Box
         as="svg"
         xmlns="http://www.w3.org/2000/svg"
@@ -48,17 +49,17 @@ const LookupField = () => {
         <path d="M7.41 7.84l4.59 4.58 4.59-4.58 1.41 1.41-6 6-6-6z" />
       </Box>
     }
-    onClick={handleClick}
-    onChange={handleChange}
-    >
-    {options.map((option, key)=>{
-      return(
-        <option key={key}>
-          {option}
-        </option>
-      )
-    })}
-    </Select>
+      onClick={handleClick}
+      onChange={handleChange}
+      >
+      {options.map((option, key)=>{
+        return (
+            <option key={key}>
+              {option}
+            </option>
+        )
+      })}
+      </Select>
   );
   
 }
