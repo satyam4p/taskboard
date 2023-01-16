cf4b5209c0ae0df0a1db38d781825278d123d5b7
satyam4p <satyamkumar343@gmail.com>
2023-01-16 23:56:38 +0530

    WIP:textEditor changes

--- src/common/TextEditor/TextEditor.js
+++ src/common/TextEditor/TextEditor.js
@@ -1,9 +1,11 @@
-import React, {useEffect, useContext, useState} from 'react';
+import React, {useEffect, useContext, useState, useCallback} from 'react';
 import {Editor} from 'react-draft-wysiwyg';
-import { EditorState } from 'draft-js';
-
+import { EditorState, ContentState, convertToRaw, convertFromRaw } from 'draft-js';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import TaskContext from '../Modals/Task/TaskContext/TaskProvider';
 import './stylesheet.scss';
+import debounce from '../../helpers/commonUtils/debounce';
+import { cloneDeep } from 'lodash';
 
 
 const TextEditor = ()=>{
@@ -11,9 +13,30 @@ const TextEditor = ()=>{
     const [editorState, setEditorState] = useState(()=>{
         EditorState.createEmpty();
     });
+    const { task, setTask } = useContext(TaskContext); 
+    
     useEffect(()=>{
-        console.log("editorState:: ",editorState)
+        console.log("editorState:: ",editorState);
+        if(editorState !== undefined){
+            const currentState = editorState.getCurrentContent();
+            console.log("EditorState:: ",convertToRaw(currentState));
+        }
     },[ editorState ]);
+
+    const updateParentState = useCallback(
+        debounce((editorState)=>{
+            setTask(prevTask=>{
+                const currentState = editorState.getCurrentContent();
+                const rawContent = convertToRaw(currentState);
+                let taskClone = cloneDeep(task);
+                taskClone.taskData['description'] = rawContent;
+                return {
+                    ...prevTask,
+                    taskData: taskClone.taskData
+                }
+                })
+            
+        },4),[])
 
     return(
         <div className= 'text-editor--container'>
