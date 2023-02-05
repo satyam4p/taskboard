65550230e321636ee7ac89da5aafe34b0dfb1746
satyam4p <satyamkumar343@gmail.com>
2023-02-05 20:35:40 +0530

    WIP(drawer): added search field and smal style changes

--- src/common/AddDrawer/AddDrawer.js
+++ src/common/AddDrawer/AddDrawer.js
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from 'react';
-
+import { Tag } from 'antd';
 import DrawerLoader from './loader/DrawerLoader';
 import { useDispatch, useSelector } from "react-redux";
+import { Input } from 'antd';
 /** custom style */
 import './stylesheet.scss';
 import { selectDrawerDetails, selectDrawerError, selectDrawerStatus } from '../../features/Drawer/drawerSlice';
+import iconsMap from '../IconsMapper/IconsMap';
+
+
+const Record = ({title, status})=>{
+
+    return(
+        <div className='record-Container'>
+            <Tag className='status-contaienr'>{status}</Tag>
+            <span className='title-container'>{title}</span>
+        </div>
+    ) 
+
+}
 
 const AddDrawer = ({showDrawer, config})=>{
 
@@ -12,15 +26,16 @@ const AddDrawer = ({showDrawer, config})=>{
     const drawerDetails = useSelector(selectDrawerDetails);
     const drawerError = useSelector(selectDrawerError);
 
-    
-
     return(
         <div className={`drawer-container ${showDrawer ? 'show' : 'hide'}`}>
+            <div className='search-container'>
+                <Input className='antd-search' placeholder='Search recent task'/>
+            </div>
             {drawerStatus === 'succeeded' ? 
                 drawerDetails.map((data,key)=>{
-                    return <p key = {key}>{data.name}</p>
+                    return <Record status={data?.status} title = {data?.name}/>
                 })
-            :<DrawerLoader/>
+            : <DrawerLoader/>
         }
             
         </div>
