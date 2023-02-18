774fd8ec3bf2fb92815bd0629186085904c82362
satyam4p <satyamkumar343@gmail.com>
2023-02-18 21:00:45 +0530

    refactor: added panels to side menu

--- src/Components/SideMenu/SideMenuV2.jsx
+++ src/Components/SideMenu/SideMenuV2.jsx
@@ -4,19 +4,26 @@ import Sidebar from "../../common/Sidebar/Sidebar";
 function SideMenu({showSideMenu, toggleAddMenu, setToggleAddMenu}){
 
     return(
+    <div>
        <Sidebar showSideMenu={showSideMenu}>
         <Sidebar.MenuList>
             <Sidebar.MenuIndex>
                 Board
             </Sidebar.MenuIndex>
-            <Sidebar.MenuIndex arrowOnHover>
+            <Sidebar.MenuIndex arrowOnHover hasPanel>
                 Recent Tasks
             </Sidebar.MenuIndex>
             <Sidebar.MenuIndex>
                 Archive
             </Sidebar.MenuIndex>
         </Sidebar.MenuList>
+        <Sidebar.SidePanels>
+            <Sidebar.SidePanel>
+                
+            </Sidebar.SidePanel>
+        </Sidebar.SidePanels>
        </Sidebar>
+    </div>
     )
 
 }
--- src/common/Sidebar/Sidebar.jsx
+++ src/common/Sidebar/Sidebar.jsx
@@ -20,9 +20,10 @@ const useSideMenuContext = ()=> {
 const Sidebar = ({ children, showSideMenu })=>{
 
     const [activeIndex, setActiveIndex] = useState(0);
+    const [panel, setPanel] = useState({})
 
     return(
-        <SideMenuContext.Provider value={{activeIndex, setActiveIndex}}>
+        <SideMenuContext.Provider value={{activeIndex, setActiveIndex, panel, setPanel}}>
             <div className={`sidemenu-container ${showSideMenu ? 'show' : 'hide'}`}>
                 { children }
             </div>
@@ -32,11 +33,37 @@ const Sidebar = ({ children, showSideMenu })=>{
 
 const MenuList = ({children})=>{
 
-    const { activeIndex, setActiveIndex } = useSideMenuContext();
-    const onActive = (index)=>setActiveIndex(index);
+    const { activeIndex, setActiveIndex, panel, setPanel} = useSideMenuContext();
+    const onActive = (index, hasPanel)=>{
+        setActiveIndex(index);
+        if(hasPanel){
+            setPanel(prev=>{
+                if(prev?.show){
+                    return {
+                        show:false,
+                        index
+                    }
+                }else{
+                    return {
+                        show: true,
+                        index
+                    }
+                }
+            })
+        }
+        if(panel?.show){
+            setPanel(prev=>{
+                return {
+                    index,
+                    show: false
+                }
+            })
+        }
+    }
+
     const Children = React.Children.map( children, (child, index)=>{
         if(child.type === MenuIndex){
-            return React.cloneElement( child, { activeIndex: activeIndex === index, onActive, index });
+            return React.cloneElement( child, { activeIndex: activeIndex === index, onActive, index});
         }else{
             return child;
         }
@@ -52,23 +79,48 @@ const MenuList = ({children})=>{
 
 Sidebar.MenuList = MenuList;
 
-const MenuIndex = ({children, activeIndex, onActive, index, arrowOnHover })=>{
+const MenuIndex = ({children, activeIndex, onActive, index, arrowOnHover, hasPanel })=>{
 
     const [arrow, setArrow] = useState(false);
     const showArrow = ()=> arrowOnHover ? arrow ? setArrow(false) : setArrow(true) : null;
 
     return(
         <div className={`menu__index ${activeIndex ? 'active' : ''}`}
-            onClick={()=>onActive(index)}
+            onClick={()=>onActive(index, hasPanel)}
             onMouseOver={()=>showArrow()} 
             onMouseLeave={()=>showArrow()}>
+
             {children}
-            {arrow ? iconsMap.doubleRightArror(16) : null}
+            {arrow ? iconsMap.doubleRightArror(14) : null}
         </div>
     )
 }
 
 Sidebar.MenuIndex = MenuIndex;
 
+const SidePanels = ({ children })=>{
+
+    const { activeIndex, panel } = useSideMenuContext();
+    console.log("panel:: ",panel);
+    return(
+        <div className={`sidemenu__sidePanels ${activeIndex && panel?.show ? 'show' : 'hide'}`}>
+            { children }
+        </div>
+    )
+}
+
+Sidebar.SidePanels = SidePanels;
+
+const SidePanel = ({ children })=>{
+
+    return(
+        <div className={`sidemenu__sidepanel`}>
+            { children }
+        </div>
+    )
+
+}
+
+Sidebar.SidePanel = SidePanel;
 
 export default Sidebar;
