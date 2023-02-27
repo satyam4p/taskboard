48abdd65a0535c33ca5e9e0cf94dc6e74ba1e891
satyam4p <satyamkumar343@gmail.com>
2023-02-27 12:22:02 +0530

    fix: fetch drawer details on task actions

--- src/helpers/hooks/useUpdateTask.js
+++ src/helpers/hooks/useUpdateTask.js
@@ -5,6 +5,7 @@ import urlSchema from '../../network/urlSchema/urlSchema.json';
 import TaskContext from "../../common/Modals/Task/TaskContext/TaskProvider";
 import useNotification from "../../common/Notification/helpers/useNotification";
 import { updateTaskBegin, updateTaskSuccess, updateTaskError } from "../../features/task/taskSlice";
+import useDrawerDetails from "./useDrawerDetails";
 
 
 const useUpdateTask =() => {
@@ -12,6 +13,7 @@ const useUpdateTask =() => {
     const show  = useNotification();
     const dispatch = useDispatch();
     const axios = useAxiosPrivate();
+    const fetchDrawerDetails = useDrawerDetails()
 
     const update = async (taskId, payload )=>{
 
@@ -29,6 +31,7 @@ const useUpdateTask =() => {
                     }
                 });
                 dispatch(updateTaskSuccess(updatedTask?.data))
+                fetchDrawerDetails("Recent Tasks");
             }
         }catch(error){
             console.log("error:: ",error);
