cdb596f8e0faef8abc6191c1d49f67c2d32c4a77
satyam4p <satyamkumar343@gmail.com>
2023-03-03 22:00:44 +0530

    WIP(theme): persist theme on client side

--- src/Components/Secondarybar/SecondaryBar.jsx
+++ src/Components/Secondarybar/SecondaryBar.jsx
@@ -1,9 +1,8 @@
 /** @jsxImportSource theme-ui */
-import { useContext } from 'react';
 import iconsMap from '../../common/IconsMapper/IconsMap';
-import ThemeContext from '../../theme/themeContext';
 import ProfileTogggle from './Profile/ProfileToggle';
 import './stylesheet.scss';
+import useTheme from '../../helpers/hooks/useTheme';
 
 
 function SecondaryBar({ 
@@ -13,35 +12,34 @@ function SecondaryBar({
     setToggleAddMenu,
     showSideMenu }){
     
-        const {theme, setTheme} = useContext(ThemeContext);
-    
+        const [changeTheme, theme] = useTheme();
 
-    return(
-        <>
-            <div className = {`secondary-bar-container ${theme === "light" ? "" : "secodnary-dark"}`}>
-                <button className='hamburger-btn'
-                        onClick={()=>{
-                        setToggleSideMenu((prev)=>!prev)
-                        setToggleAddMenu(false)
-                    }}>
-                        <div className={`bar bar1 ${showSideMenu ? 'showSideMenu' : '' }`}/>
-                        <div className={`bar bar2 ${showSideMenu ? 'showSideMenu' : '' }`} />
-                        <div className={`bar bar3 ${showSideMenu ? 'showSideMenu' : '' }`} />
-                </button>
-                <div className='right-section'>
-                    <button className='theme-change-btn' onClick={()=>setTheme( theme === "light" ? "dark" : "light")}>
-                        {iconsMap.theme(24)}
+        return(
+            <>
+                <div className = {`secondary-bar-container ${theme === "light" ? "" : "secodnary-dark"}`}>
+                    <button className='hamburger-btn'
+                            onClick={()=>{
+                            setToggleSideMenu((prev)=>!prev)
+                            setToggleAddMenu(false)
+                        }}>
+                            <div className={`bar bar1 ${showSideMenu ? 'showSideMenu' : '' }`}/>
+                            <div className={`bar bar2 ${showSideMenu ? 'showSideMenu' : '' }`} />
+                            <div className={`bar bar3 ${showSideMenu ? 'showSideMenu' : '' }`} />
                     </button>
-                    <button className='profile-btn'
-                            onClick={()=>setToggleProile(!toggleProfile)} />
+                    <div className='right-section'>
+                        <button className='theme-change-btn' onClick={()=>changeTheme()}>
+                            {iconsMap.theme(24)}
+                        </button>
+                        <button className='profile-btn'
+                                onClick={()=>setToggleProile(!toggleProfile)} />
+                    </div>
+                    
                 </div>
-                
-            </div>
 
-            {toggleProfile && <ProfileTogggle />}
-        </>
+                {toggleProfile && <ProfileTogggle />}
+            </>
 
-    )
+        )
 }
 
 
--- src/theme/themeContext.js
+++ src/theme/themeContext.js
@@ -1,11 +1,18 @@
-import { createContext, useState } from "react";
-
+import { createContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext({});
 
 export const ThemeProvider = ({children}) => {
 
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState();
+
+    useEffect(()=>{
+
+        const currentTheme = window.localStorage.getItem("taskboard-theme");
+
+        setTheme(currentTheme);
+
+    },[theme, setTheme]);
 
     return(
         <ThemeContext.Provider value={{theme, setTheme}}>
