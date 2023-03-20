db9fb17e9999281e7a64ab499b6b16b45b94d343
satyam4p <satyamkumar343@gmail.com>
2023-03-20 22:14:08 +0530

    WIP(board): board slice added

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import taskReducer from '../features/task/taskSlice';
 import drawerReducer from '../features/Drawer/drawerSlice';
+import BoardReducer from '../features/Board/BoardSlice';
 
 
 export const store = configureStore({
     reducer:{
         task: taskReducer,
         drawer: drawerReducer,
+        board: BoardReducer,
         comments:{},
         user:{}
     }
