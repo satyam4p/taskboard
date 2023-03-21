a89aa333b57b6692438128b6bb2e8f2b298a3965
satyam4p <satyamkumar343@gmail.com>
2023-03-21 23:34:14 +0530

    WIP(board): hook for all tasks

---
+++ src/helpers/hooks/useTasks.js
@@ -0,0 +1,28 @@
+import { useCallback } from "react";
+import { fetchBoardTasksBegin, fetchBoardtasksSuccess, fetchBoardTasksError } from "../../features/Board/BoardSlice";
+import useAxiosPrivate from "./useAxiosPrivate";
+import { useDispatch } from "react-redux";
+import urlSchema from '../../network/urlSchema/urlSchema.json';
+
+const useTasks = ()=>{
+    const dispatch = useDispatch();
+    const axiosprivate = useAxiosPrivate();
+    const fetchTasks = useCallback(async () =>{
+        const URL = urlSchema.Tasks.FETCH_ALL;
+        console.log("URL:: ",URL);
+        try{
+            dispatch(fetchBoardTasksBegin); 
+            const result = await axiosprivate.get(URL);
+            if(result && result.data){
+                const tasks = Object.values(result.data);
+                console.log("tasks:: ",tasks);
+                dispatch(fetchBoardtasksSuccess(tasks));
+            }
+        }catch(error){
+            dispatch(fetchBoardTasksError(error));
+        }
+    },[ axiosprivate, dispatch ]) 
+    return fetchTasks;
+}
+
+export default useTasks;
