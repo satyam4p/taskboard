01f30ad42c8c9ef1d0e05b5560195b9a395072e7
satyam4p <satyamkumar343@gmail.com>
2023-04-02 16:41:08 +0530

    WIP(board):theme changes

--- src/Components/BoardLayout/BoardLayout.jsx
+++ src/Components/BoardLayout/BoardLayout.jsx
@@ -4,6 +4,8 @@ import Column from 'antd/lib/table/Column';
 import './stylesheet.scss';
 import BoardActions from './BoardActions/BoardActions';
 import composedTableLayout from './WithLayout/WithTable/WithTableLayout';
+import ThemeContext from '../../theme/themeContext';
+import { useContext } from 'react';
 
 const BoardLayout = (props)=>{
 
@@ -12,6 +14,7 @@ const BoardLayout = (props)=>{
     console.log("boardTasks:: ",boardTasks);
     console.log("boardTasks:: ",boardTasks);
     console.log("Type:: ",type);
+    const { theme } = useContext(ThemeContext);
     return(
         <div className='board-layout'>
             {/* <BoardActions/>    */}
@@ -39,7 +42,7 @@ const BoardLayout = (props)=>{
                          {boardTasks && boardTasks.length 
                             ? boardTasks.map((task, key)=>{
                                     return(
-                                        <tr className='table-row'>
+                                        <tr className={`table-row ${theme}`}>
                                             <td className='sequence'>
                                                 {key+1}
                                             </td>
@@ -47,7 +50,7 @@ const BoardLayout = (props)=>{
                                                 {task.name}
                                             </td>
                                             <td className='status'>
-                                                <div className={`status-value ${task.status.trim().split(" ").join("-").toLowerCase()}`}>
+                                                <div className={`status-value ${task.status.trim().split(" ").join("-").toLowerCase()} ${theme}`}>
                                                     {task.status}
                                                 </div>
                                                 
@@ -56,7 +59,7 @@ const BoardLayout = (props)=>{
                                                 {task.assignee}
                                             </td>
                                             <td className='label'>
-                                                <div className={`label-value ${task.label.trim().split(" ").join("-").toLowerCase()}`}>
+                                                <div className={`label-value ${task.label.trim().split(" ").join("-").toLowerCase()} ${theme}`}>
                                                     {task.label}
                                                 </div>
                                             </td>
