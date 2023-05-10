d6e205f0f2b5b9c8cf9f0fa56edcb6d60c35b3c2
satyam4p <satyamkumar343@gmail.com>
2023-05-10 16:15:33 +0530

    WIP: profile sie changes

--- src/views/Profile/profile.jsx
+++ src/views/Profile/profile.jsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
 import useProfile from "../../helpers/hooks/useProfile";
+import { selectUserProfile, selectStatus, selectError } from "../../features/profile/profileSlice";
+import { useSelector } from "react-redux";
+import './stylesheet.scss';
 
 const UserProfile = ( props )=>{
-
+    const userProfile = useSelector(selectUserProfile);
+    const profileStatus = useSelector(selectStatus);
+    const profileError = useSelector(selectError);
     const [getUserProfile, profileCallEnded] = useProfile();
     useEffect(()=>{
        getUserProfile();
     },[profileCallEnded]);
 
     return(
-        <div>
-            User Profile
+        <div className="user-profile">
+            <div className="user-profile-image-container">
+                <img className="user-profile-image" src={`${userProfile?.profile}`} alt="profile"/>
+            </div>
+            
         </div> 
     )
 }   
