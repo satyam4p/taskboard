cf70e56485b1294611b7001e49e0ae8a9ff07265
satyam4p <satyamkumar343@gmail.com>
2023-08-09 23:54:02 +0530

    WIP

--- src/views/Profile/profile.jsx
+++ src/views/Profile/profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import useProfile from "../../helpers/hooks/useProfile";
 import { selectUserProfile, selectStatus, selectError } from "../../features/profile/profileSlice";
 import { useSelector } from "react-redux";
